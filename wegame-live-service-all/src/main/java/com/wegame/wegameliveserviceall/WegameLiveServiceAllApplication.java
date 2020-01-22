31e400f70fdbb608bf81a39aebb398542a283284
wangbaihong <136207024@qq.com>
2020-01-22 11:17:49 +0800

    first commit

---
+++ wegame-live-service-all/src/main/java/com/wegame/wegameliveserviceall/WegameLiveServiceAllApplication.java
@@ -0,0 +1,13 @@
+package com.wegame.wegameliveserviceall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WegameLiveServiceAllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WegameLiveServiceAllApplication.class, args);
+    }
+
+}
---
+++ wegame-live-service-all/src/test/java/com/wegame/wegameliveserviceall/WegameLiveServiceAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wegame.wegameliveserviceall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WegameLiveServiceAllApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
