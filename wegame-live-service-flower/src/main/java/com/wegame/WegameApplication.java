b87e4e232548a5e011c536c48dca8a19b4a7ccb4
wbh <136207024@qq.com>
2020-05-06 16:19:18 +0800

    首次提交

---
+++ wegame-live-service-flower/src/main/java/com/wegame/WegameApplication.java
@@ -0,0 +1,13 @@
+package com.wegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WegameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WegameApplication.class, args);
+    }
+
+}
