945d51d85b72c62ea4e9fa1a66e4aea0f8ad52cf
wangbaihong <136207024@qq.com>
2020-09-07 23:01:32 +0800

    添加日志打印 修改页面，  等待处理控制台打印+1

---
+++ wegame-live-service-flower/src/main/java/com/wegame/config/BaseController.java
@@ -0,0 +1,15 @@
+package com.wegame.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @ClassName：BaseController
+ * @Description: 基类
+ * @Author：13738700108
+ * @Data 2020/9/7 20:25
+ * @Version: v1.0
+ **/
+public class BaseController {
+    public static final Logger LOGGER = LoggerFactory.getLogger(BaseController.class);
+}
